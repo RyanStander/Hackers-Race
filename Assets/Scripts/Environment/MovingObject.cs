e0f004e8e936bccd6c4e4d3699de07ea76d20361
Ryan <stander.ryan@gmail.com>
2025-04-08 02:01:48 +0200

    Moving platforms and shifting walls

---
+++ Assets/Scripts/Environment/MovingObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Environment
+{
+    public class MovingObject : MonoBehaviour
+    {
+        [SerializeField] float speed = 1.0f;
+        private Vector3 startingPosition;
+        private Vector3 endingPosition;
+        [SerializeField] private Vector3 moveAmounts;
+        private bool hasReachedEnd = false;
+        private void Start()
+        {
+            startingPosition = transform.position;
+            endingPosition = startingPosition + moveAmounts;
+        }
+
+        private void FixedUpdate()
+        {
+            if (hasReachedEnd)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startingPosition, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, startingPosition) < 0.01f)
+                {
+                    hasReachedEnd = false;
+                }
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endingPosition, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, endingPosition) < 0.01f)
+                {
+                    hasReachedEnd = true;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,117 +1,137 @@
 using System;
+using Environment;
 using UnityEngine;
 
 namespace Player
 {
-	/// <summary>
-	/// Script is based on script from youtuber Alpharoah on a specific video: https://www.youtube.com/watch?v=AVjbCn5i_rk
-	/// Has been addapted to better suit my preferences for how it should function but contains the same core functionality.
-	/// </summary>
-	public class PlayerMovement : MonoBehaviour
-	{
-		[SerializeField] private PlayerManager playerManager;
-		[SerializeField] private float moveSpeed = 7.0f; // Ground move speed
-		[SerializeField] private float sprintMultiplier = 2f; // Ground accel
+    /// <summary>
+    /// Script is based on script from youtuber Alpharoah on a specific video: https://www.youtube.com/watch?v=AVjbCn5i_rk
+    /// Has been addapted to better suit my preferences for how it should function but contains the same core functionality.
+    /// </summary>
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private PlayerManager playerManager;
+        [SerializeField] private float moveSpeed = 7.0f; // Ground move speed
+        [SerializeField] private float sprintMultiplier = 2f; // Ground accel
 
-		[SerializeField] private float groundCheckDistance = 0.4f;
-		[SerializeField] private LayerMask groundMask;
-		[SerializeField, Range(0,1)] private float icyness = 0.5f;
-		
-		private PlayerController playerController;
-		private Rigidbody playerRigidbody;
-		
-		private float desiredSpeed;
-		
-		[Range(0,4)]public float JumpHeight = 1f;
-		private const float jumpHeightMultiplier = 5;
+        [SerializeField] private float groundCheckDistance = 0.4f;
+        [SerializeField] private LayerMask groundMask;
+        [SerializeField, Range(0, 1)] private float icyness = 0.5f;
 
-		public bool JumpQueue;
-		public bool WishJump;
-		
-		private Vector3 desiredDirection;
-		private Vector3 playerMovement;
+        private PlayerController playerController;
+        private Rigidbody playerRigidbody;
 
-		public bool IsGrounded;
-		
-		public delegate void CanBreakBlockades();
-		public static CanBreakBlockades HighSpeedAchieved;
-		public static CanBreakBlockades ReturnedToLowSpeed;
-		private bool isInHighSpeed;
+        private float desiredSpeed;
 
-		private void OnValidate()
-		{
-			if (playerManager == null)
-				playerManager = GetComponent<PlayerManager>();
-		}
+        [Range(0, 4)] public float JumpHeight = 1f;
+        private const float jumpHeightMultiplier = 5;
 
-		private void Start()
-		{
-			playerController = playerManager.PlayerController;
-			playerRigidbody = playerManager.Rigidbody;
-		}
+        public bool JumpQueue;
+        public bool WishJump;
 
-		private void FixedUpdate()
-		{
-			playerRigidbody.AddForce(-playerRigidbody.velocity*icyness + playerMovement);
-			
-			if (WishJump)
-			{
-				playerRigidbody.AddForce(Vector3.up * (JumpHeight * jumpHeightMultiplier), ForceMode.Impulse);
-				WishJump = false;
-			}
+        private Vector3 desiredDirection;
+        private Vector3 playerMovement;
 
-			if (playerRigidbody.velocity.magnitude > 30 && !isInHighSpeed)
-			{
-				HighSpeedAchieved?.Invoke();
-				isInHighSpeed = true;
-			}
-			else if (playerRigidbody.velocity.magnitude < 30 && isInHighSpeed)
-			{
-				ReturnedToLowSpeed?.Invoke();
-				isInHighSpeed = false;
-			}
+        public bool IsGrounded;
 
-		}
+        public delegate void CanBreakBlockades();
 
-		public void HandleMovement()
-		{
-			IsGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
-			QueueJump();
-			
-			Movement();
-		}
-		
-		private void Movement()
-		{
-			desiredDirection = new Vector3(playerController.Left, 0, playerController.Forward);
-			desiredDirection = transform.TransformDirection(desiredDirection);
+        public static CanBreakBlockades HighSpeedAchieved;
+        public static CanBreakBlockades ReturnedToLowSpeed;
+        private bool isInHighSpeed;
 
-			playerMovement = desiredDirection * moveSpeed;
+        private void OnValidate()
+        {
+            if (playerManager == null)
+                playerManager = GetComponent<PlayerManager>();
+        }
 
-			if (playerController.SprintInput)
-			{
-				playerMovement *= sprintMultiplier;
-			}
-		}
-		
-		//Queues the next jump
-		private void QueueJump()
-		{
-			if (playerController.JumpInput && IsGrounded)
-			{
-				WishJump = true;
-			}
+        private void Start()
+        {
+            playerController = playerManager.PlayerController;
+            playerRigidbody = playerManager.Rigidbody;
+        }
 
-			if (!IsGrounded && playerController.JumpInput)
-			{
-				JumpQueue = true;
-			}
+        private void FixedUpdate()
+        {
+            playerRigidbody.AddForce(-playerRigidbody.velocity * icyness + playerMovement);
 
-			if (IsGrounded && JumpQueue)
-			{
-				WishJump = true;
-				JumpQueue = false;
-			}
-		}
-	}
+            if (WishJump)
+            {
+                playerRigidbody.AddForce(Vector3.up * (JumpHeight * jumpHeightMultiplier), ForceMode.Impulse);
+                WishJump = false;
+            }
+
+            if (playerRigidbody.velocity.magnitude > 30 && !isInHighSpeed)
+            {
+                HighSpeedAchieved?.Invoke();
+                isInHighSpeed = true;
+            }
+            else if (playerRigidbody.velocity.magnitude < 30 && isInHighSpeed)
+            {
+                ReturnedToLowSpeed?.Invoke();
+                isInHighSpeed = false;
+            }
+        }
+
+        public void HandleMovement()
+        {
+            IsGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+            QueueJump();
+
+            Movement();
+
+            CheckForMovingPlatform();
+        }
+
+        private void Movement()
+        {
+            desiredDirection = new Vector3(playerController.Left, 0, playerController.Forward);
+            desiredDirection = transform.TransformDirection(desiredDirection);
+
+            playerMovement = desiredDirection * moveSpeed;
+
+            if (playerController.SprintInput)
+            {
+                playerMovement *= sprintMultiplier;
+            }
+        }
+
+        //Queues the next jump
+        private void QueueJump()
+        {
+            if (playerController.JumpInput && IsGrounded)
+            {
+                WishJump = true;
+            }
+
+            if (!IsGrounded && playerController.JumpInput)
+            {
+                JumpQueue = true;
+            }
+
+            if (IsGrounded && JumpQueue)
+            {
+                WishJump = true;
+                JumpQueue = false;
+            }
+        }
+
+        private void CheckForMovingPlatform()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, groundCheckDistance, groundMask) &&
+                hit.transform.CompareTag("MovingPlatform") && hit.transform.GetComponent<MovingObject>())
+            {
+                transform.SetParent(hit.transform, true);
+            }
+            else
+            {
+                if (transform.parent != null)
+                {
+                    transform.SetParent(null, true);
+                }
+            }
+        }
+    }
 }
